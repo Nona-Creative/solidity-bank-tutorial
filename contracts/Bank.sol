f2b9870dcb86c98e310d46ee34dbf0bb6a391e12
Neil Russell <neilrussell6@gmail.com>
2019-02-01 07:39:02 +0200

    feat: solve challenge 1.

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -1,12 +1,31 @@
 pragma solidity ^0.4.24;
 
+import "./common/libs/SafeMath.sol";
+
 
 contract Bank {
+  using SafeMath for uint256;
+
+  // ------------------------------------------
+  // properties
+  // ------------------------------------------
+
   mapping(address => bool) private accounts;
   mapping(address => uint256) private balances;
   uint256 public createAccountFee = 1000;
 
+  // ------------------------------------------
+  // events
+  // ------------------------------------------
+
   event AccountCreated(address accountAddress);
+  event Deposit(address accountAddress, uint256 amount, uint256 balance);
+  event Withdraw(address accountAddress, uint256 amount, uint256 balance);
+  event Transfer(address fromAccountAddress, address toAccountAddress, uint256 amount, uint256 balance);
+
+  // ------------------------------------------
+  // external API
+  // ------------------------------------------
 
   function isMyAccountActive() external view returns (bool) {
     return accounts[msg.sender];
@@ -14,7 +33,53 @@ contract Bank {
 
   function createAccount() external payable {
     require(msg.value == createAccountFee, "Please provide the correct account creation fee");
-    accounts[msg.sender] = true; // create and activate account
+    activateAccount(msg.sender); // activate account
     emit AccountCreated(msg.sender);
+  }
+
+  function myBalance() external view returns (uint256) {
+    require(accounts[msg.sender], "Please create an account first");
+    return balances[msg.sender];
+  }
+
+  function deposit() external payable {
+    require(accounts[msg.sender], "Please create an account first");
+    incBalance(msg.sender, msg.value);
+    emit Deposit(msg.sender, msg.value, balances[msg.sender]);
+  }
+
+  // NOTE: All the fees of a transaction (including any internal transactions it triggers)
+  // are paid by the transaction sender. Contracts cannot pay gas fees.
+  function withdraw(uint256 amount) external payable {
+    require(accounts[msg.sender], "Please create an account first");
+    require(balances[msg.sender] >= amount, "Insufficient funds");
+    msg.sender.transfer(amount);
+    decBalance(msg.sender, amount);
+    emit Withdraw(msg.sender, amount, balances[msg.sender]);
+  }
+
+  function transfer(address toAccount, uint256 amount) external {
+    require(accounts[msg.sender], "Please create an account first");
+    require(balances[msg.sender] >= amount, "Insufficient funds");
+    require(accounts[toAccount], "Invalid target account");
+    incBalance(toAccount, amount);
+    decBalance(msg.sender, amount);
+    emit Transfer(msg.sender, toAccount, amount, balances[msg.sender]);
+  }
+
+  // ------------------------------------------
+  // internal utilities
+  // ------------------------------------------
+
+  function incBalance(address account, uint256 amount) internal {
+    balances[account] = balances[account].add(amount);
+  }
+
+  function decBalance(address account, uint256 amount) internal {
+    balances[account] = balances[account].sub(amount);
+  }
+
+  function activateAccount(address account) internal {
+    accounts[account] = true;
   }
 }
--- contracts/Bank.test.js
+++ contracts/Bank.test.js
@@ -1,6 +1,8 @@
 const chai = require('chai')
 const { ContractUtils } = require('ethereum-utils')
 const chaiAsPromised = require('chai-as-promised')
+const Web3 = require('web3')
+const BigNumber = require('bignumber.js')
 
 const { Contract } = require('../src/common/utils/ethereum-test-utils')
 
@@ -14,6 +16,11 @@ describe('Bank', () => {
     it('should expose public properties and methods', contract('Bank', async ({ accounts, instance }) => {
       assert.includeMembers(Object.keys(instance.methods), [
         'createAccountFee',
+        'createAccount',
+        'myBalance',
+        'deposit',
+        'withdraw',
+        'transfer',
       ])
     }))
 
@@ -21,6 +28,9 @@ describe('Bank', () => {
       assert.notIncludeMembers(Object.keys(instance.methods), [
         'accounts',
         'balances',
+        'incBalance',
+        'decBalance',
+        'activateAccount',
       ])
     }))
   })
@@ -64,7 +74,7 @@ describe('Bank', () => {
       )
       const tx = await ContractUtils.send(
         instance.methods.createAccount(),
-        { from: accounts[0], value: createAccountFee },
+        { from: accounts[0], value: Web3.utils.toHex(createAccountFee) },
         true,
       )
 
@@ -80,7 +90,7 @@ describe('Bank', () => {
       assert.hasAllKeys(tx.events, ['AccountCreated'])
       const AccountCreatedEvents = ContractUtils.events('AccountCreated', tx)
       assert.equal(AccountCreatedEvents.length, 1)
-      assert.property(AccountCreatedEvents[0], 'accountAddress')
+      assert.equal(AccountCreatedEvents[0].accountAddress.toLowerCase(), accounts[0])
     }))
 
     it('should reject as expected if we create account with incorrect fee', contract('Bank', async ({ accounts, instance }) => {
@@ -89,11 +99,360 @@ describe('Bank', () => {
       await assert.isRejected(
         ContractUtils.send(
           instance.methods.createAccount(),
-          { from: accounts[0], value: 0 },
+          { from: accounts[0], value: Web3.utils.toHex(0) },
           true,
         ),
         Error,
         /Please provide the correct account creation fee/,
+      )
+    }))
+  })
+
+  describe('myBalance', () => {
+    it('should return the balance of the our account', contract('Bank', async ({ accounts, instance }) => {
+      // given
+      // ... an active account exists for address 0 with a balance of 123
+      const createAccountFee = await ContractUtils.call(
+        instance.methods.createAccountFee(),
+        { from: accounts[2] },
+      )
+      await ContractUtils.send(
+        instance.methods.createAccount(),
+        { from: accounts[0], value: Web3.utils.toHex(createAccountFee) },
+        true,
+      )
+      await ContractUtils.send(
+        instance.methods.deposit(),
+        { from: accounts[0], value: Web3.utils.toHex(123) },
+        true,
+      )
+      // when
+      // ... we check the balance for the account owned by address 0
+      const balance = await ContractUtils.call(
+        instance.methods.myBalance(),
+        { from: accounts[0] },
+      )
+
+      // then ... should return the correct balance
+      assert.equal(balance, 123)
+    }))
+
+    it('should reject as expected if we attempt to check our account balance when we do not have an account', contract('Bank', async ({ accounts, instance }) => {
+      // given ... no / or inactive account exists for address 0
+      // when ... we check the balance for the address 0's account
+      // then ... should reject as expected
+      await assert.isRejected(
+        ContractUtils.send(
+          instance.methods.myBalance(),
+          { from: accounts[0] },
+          true,
+        ),
+        Error,
+        /Please create an account first/,
+      )
+    }))
+  })
+
+  describe('deposit', () => {
+    it('should increment the balance of our account as expected', contract('Bank', async ({ web3, accounts, instance }) => {
+      // given ... an active account exists for address 0 with a balance of 0
+      const createAccountFee = await ContractUtils.call(
+        instance.methods.createAccountFee(),
+        { from: accounts[2] },
+      )
+      await ContractUtils.send(
+        instance.methods.createAccount(),
+        { from: accounts[0], value: Web3.utils.toHex(createAccountFee) },
+        true,
+      )
+
+      // when ... we deposit 123 Ether into the address 0's account
+      const account0BalanceBefore = new BigNumber(await web3.eth.getBalance(accounts[0]))
+      const tx = await ContractUtils.send(
+        instance.methods.deposit(),
+        { from: accounts[0], value: Web3.utils.toHex(123) },
+        true,
+      )
+
+      // then
+      // ... should have cost address 0 123 wei after gas fees
+      const account0BalanceAfter = new BigNumber(await web3.eth.getBalance(accounts[0]))
+      const txGasPriceWei = Web3.utils.toWei('6.2', 'gwei')
+      const txGasCost = tx.cumulativeGasUsed * txGasPriceWei
+      const account0BalanceDiff = account0BalanceBefore.minus(account0BalanceAfter).minus(txGasCost).toNumber()
+      assert.equal(account0BalanceDiff, 123)
+
+      // ... should have incremented address 0's account balance as expected
+      const balance = await ContractUtils.call(
+        instance.methods.myBalance(),
+        { from: accounts[0] },
+      )
+      assert.equal(balance, 123)
+
+      // ... should have emitted a Deposit event with expected values
+      assert.hasAllKeys(tx.events, ['Deposit'])
+      const DepositEvents = ContractUtils.events('Deposit', tx)
+      assert.equal(DepositEvents.length, 1)
+      assert.equal(DepositEvents[0].accountAddress.toLowerCase(), accounts[0])
+      assert.equal(DepositEvents[0].amount, '123')
+      assert.equal(DepositEvents[0].balance, '123')
+    }))
+
+    it('should reject as expected if we attempt to deposit without an active account', contract('Bank', async ({ accounts, instance }) => {
+      // given ... no / or inactive account exists for address 0
+      // when ... we deposit 123 Eth into address 0's account
+      // then ... should reject as expected
+      await assert.isRejected(
+        ContractUtils.send(
+          instance.methods.deposit(),
+          { from: accounts[0], value: Web3.utils.toHex(123) },
+          true,
+        ),
+        Error,
+        /Please create an account first/,
+      )
+    }))
+  })
+
+  describe('withdraw', () => {
+    it('should decrement the balance of our account as expected', contract('Bank', async ({ web3, accounts, instance }) => {
+      // given ... an active account exists for address 0 with a balance of 123
+      const createAccountFee = await ContractUtils.call(
+        instance.methods.createAccountFee(),
+        { from: accounts[2] },
+      )
+      await ContractUtils.send(
+        instance.methods.createAccount(),
+        { from: accounts[0], value: Web3.utils.toHex(createAccountFee) },
+        true,
+      )
+      await ContractUtils.send(
+        instance.methods.deposit(),
+        { from: accounts[0], value: Web3.utils.toHex(123) },
+        true,
+      )
+
+      // when ... we withdraw 24 Ether from address 0's account
+      const account0BalanceBefore = new BigNumber(await web3.eth.getBalance(accounts[0]))
+      const tx = await ContractUtils.send(
+        instance.methods.withdraw(24),
+        { from: accounts[0] },
+        true,
+      )
+
+      // then
+      // ... should have credited address 0's with 123 wei excluding transaction gas fees
+      const account0BalanceAfter = new BigNumber(await web3.eth.getBalance(accounts[0]))
+      const txGasPriceWei = Web3.utils.toWei('6.2', 'gwei')
+      const txGasCost = tx.cumulativeGasUsed * txGasPriceWei
+      const account0BalanceDiff = account0BalanceBefore.minus(account0BalanceAfter).minus(txGasCost).toNumber()
+      assert.equal(account0BalanceDiff, -24)
+
+      // ... should have decremented address 0's account balance as expected
+      const balance = await ContractUtils.call(
+        instance.methods.myBalance(),
+        { from: accounts[0] },
+      )
+      assert.equal(balance, 99)
+
+      // ... should have emitted a Withdraw event with expected values
+      assert.hasAllKeys(tx.events, ['Withdraw'])
+      const WithdrawEvents = ContractUtils.events('Withdraw', tx)
+      assert.equal(WithdrawEvents.length, 1)
+      assert.equal(WithdrawEvents[0].accountAddress.toLowerCase(), accounts[0])
+      assert.equal(WithdrawEvents[0].amount, '24')
+      assert.equal(WithdrawEvents[0].balance, '99')
+    }))
+
+    it('should reject as expected if we attempt to withdraw without an active account', contract('Bank', async ({ accounts, instance }) => {
+      // given ... no / or inactive account exists for address 0
+      // when ... we withdraw 24 Eth from address 0's account
+      // then ... should reject as expected
+      await assert.isRejected(
+        ContractUtils.send(
+          instance.methods.withdraw(24),
+          { from: accounts[0] },
+          true,
+        ),
+        Error,
+        /Please create an account first/,
+      )
+    }))
+
+    it('should reject as expected if we attempt to withdraw more funds than are available', contract('Bank', async ({ accounts, instance }) => {
+      // given ... an active account exists for address 0 with a 27 Eth balance
+      const createAccountFee = await ContractUtils.call(
+        instance.methods.createAccountFee(),
+        { from: accounts[2] },
+      )
+      await ContractUtils.send(
+        instance.methods.createAccount(),
+        { from: accounts[0], value: Web3.utils.toHex(createAccountFee) },
+        true,
+      )
+      await ContractUtils.send(
+        instance.methods.deposit(),
+        { from: accounts[0], value: Web3.utils.toHex(27) },
+        true,
+      )
+
+      // when ... we withdraw 30 Eth from address 0's account
+      // then ... should reject as expected
+      await assert.isRejected(
+        ContractUtils.send(
+          instance.methods.withdraw(30),
+          { from: accounts[0] },
+          true,
+        ),
+        Error,
+        /Insufficient funds/,
+      )
+    }))
+  })
+
+  describe('transfer', () => {
+    it('should transfer requested Eth amount from our account to target account', contract('Bank', async ({ web3, accounts, instance }) => {
+      // given
+      const createAccountFee = await ContractUtils.call(
+        instance.methods.createAccountFee(),
+        { from: accounts[2] },
+      )
+      // ... an active account exists for address 0 with a balance of 123
+      await ContractUtils.send(
+        instance.methods.createAccount(),
+        { from: accounts[0], value: Web3.utils.toHex(createAccountFee) },
+        true,
+      )
+      await ContractUtils.send(
+        instance.methods.deposit(),
+        { from: accounts[0], value: Web3.utils.toHex(123) },
+        true,
+      )
+      // ... and an active account exists for address 2 with a balance of 10
+      await ContractUtils.send(
+        instance.methods.createAccount(),
+        { from: accounts[2], value: Web3.utils.toHex(createAccountFee) },
+        true,
+      )
+      await ContractUtils.send(
+        instance.methods.deposit(),
+        { from: accounts[2], value: Web3.utils.toHex(10) },
+        true,
+      )
+
+      // when ... we transfer 24 Ether from address 0's account to address 1's account
+      const tx = await ContractUtils.send(
+        instance.methods.transfer(accounts[2], 24),
+        { from: accounts[0] },
+        true,
+      )
+
+      // then
+      // ... should have decremented address 0's account balance as expected
+      const account0Balance = await ContractUtils.call(
+        instance.methods.myBalance(),
+        { from: accounts[0] },
+      )
+      assert.equal(account0Balance, 99)
+
+      // ... should have incremented address 2's account balance as expected
+      const account2Balance = await ContractUtils.call(
+        instance.methods.myBalance(),
+        { from: accounts[2] },
+      )
+      assert.equal(account2Balance, 34)
+
+      // ... should have emitted a Transfer event with expected values
+      assert.hasAllKeys(tx.events, ['Transfer'])
+      const TransferEvents = ContractUtils.events('Transfer', tx)
+      assert.equal(TransferEvents.length, 1)
+      assert.equal(TransferEvents[0].fromAccountAddress.toLowerCase(), accounts[0])
+      assert.equal(TransferEvents[0].toAccountAddress.toLowerCase(), accounts[2])
+      assert.equal(TransferEvents[0].amount, '24')
+      assert.equal(TransferEvents[0].balance, '99')
+    }))
+
+    it('should reject as expected if we attempt to transfer without an active account', contract('Bank', async ({ accounts, instance }) => {
+      // given ... no / or inactive account exists for address 0
+      // when ... we transfer 24 Eth from address 0's account to anywhere
+      // then ... should reject as expected
+      await assert.isRejected(
+        ContractUtils.send(
+          instance.methods.transfer(accounts[2], 24),
+          { from: accounts[0] },
+          true,
+        ),
+        Error,
+        /Please create an account first/,
+      )
+    }))
+
+    it('should reject as expected if we attempt to transfer more funds than are available', contract('Bank', async ({ accounts, instance }) => {
+      // given
+      const createAccountFee = await ContractUtils.call(
+        instance.methods.createAccountFee(),
+        { from: accounts[2] },
+      )
+      // ... an active account exists for address 0 with a 27 Eth balance
+      await ContractUtils.send(
+        instance.methods.createAccount(),
+        { from: accounts[0], value: Web3.utils.toHex(createAccountFee) },
+        true,
+      )
+      await ContractUtils.send(
+        instance.methods.deposit(),
+        { from: accounts[0], value: Web3.utils.toHex(27) },
+        true,
+      )
+      // ... and an active account exists for address 2 with a zero balance
+      await ContractUtils.send(
+        instance.methods.createAccount(),
+        { from: accounts[2], value: Web3.utils.toHex(createAccountFee) },
+        true,
+      )
+
+      // when ... we attempt to transfer 30 Eth from address 0's account
+      // then ... should reject as expected
+      await assert.isRejected(
+        ContractUtils.send(
+          instance.methods.transfer(accounts[2], 30),
+          { from: accounts[0] },
+          true,
+        ),
+        Error,
+        /Insufficient funds/,
+      )
+    }))
+
+    it('should reject as expected if we attempt to transfer funds to a non-existing / inactive account', contract('Bank', async ({ accounts, instance }) => {
+      // given
+      // ... an active account exists for address 0 with a 123 Eth balance
+      const createAccountFee = await ContractUtils.call(
+        instance.methods.createAccountFee(),
+        { from: accounts[2] },
+      )
+      await ContractUtils.send(
+        instance.methods.createAccount(),
+        { from: accounts[0], value: Web3.utils.toHex(createAccountFee) },
+        true,
+      )
+      await ContractUtils.send(
+        instance.methods.deposit(),
+        { from: accounts[0], value: Web3.utils.toHex(123) },
+        true,
+      )
+      // ... but no active account exists for address 2
+
+      // when ... we attempt to transfer 30 Eth from address 0's account to address 2's account
+      // then ... should reject as expected
+      await assert.isRejected(
+        ContractUtils.send(
+          instance.methods.transfer(accounts[2], 30),
+          { from: accounts[0] },
+          true,
+        ),
+        Error,
+        /Invalid target account/,
       )
     }))
   })
